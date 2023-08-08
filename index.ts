1d13ddf95786d63474a19819e948e8c350ce4010
Reza <rshahazegh@gmail.com>
2023-08-08 14:04:18 +0330

    git init

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(3000, () => {
+    console.log(`[server]: Server is running at http://localhost:3000`);
+});
